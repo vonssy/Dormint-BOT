d05cabb13871b6eb41c816368c9a49bafa89a298
vonssy <kak.reyvaldizakaria@gmail.com>
2024-11-10 17:22:50 +0700

    added save and renew token

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import os
-import urllib.parse
+from urllib.parse import parse_qs, unquote
 from colorama import *
 from datetime import datetime, timedelta
 import time
@@ -16,7 +16,7 @@ class Dormint:
             'Accept': 'application/json, text/plain, */*',
             'Accept-Language': 'en-US,en;q=0.9',
             'Cache-Control': 'no-cache',
-            'Host': 'api-new.dormint.io',
+            'Host': 'gate.api-dormint.com',
             'Origin': 'https://web.dormint.io',
             'Pragma': 'no-cache',
             'Referer': 'https://web.dormint.io/',
@@ -51,17 +51,139 @@ class Dormint:
         minutes, seconds = divmod(remainder, 60)
         return f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}"
     
-    def load_data(self, query: str):
-        query_params = urllib.parse.parse_qs(query)
-        query = query_params.get('user', [None])[0]
+    def extract_user_data(self, query: str) -> str:
+        parsed_query = parse_qs(query)
+        user_data = parsed_query.get('user', [None])[0]
 
-        if query:
-            user_data_json = urllib.parse.unquote(query)
-            user_data = json.loads(user_data_json)
-            first_name = user_data.get('first_name', 'Unknown')
-            return first_name
-        else:
-            raise ValueError("User data not found in query.")
+        if user_data:
+            user_json = json.loads(unquote(user_data))
+            return str(user_json.get('first_name', 'Unknown'))
+        return 'Unknown'
+
+    def load_tokens(self):
+        try:
+            if not os.path.exists('tokens.json'):
+                return {"accounts": []}
+
+            with open('tokens.json', 'r') as file:
+                data = json.load(file)
+                if "accounts" not in data:
+                    return {"accounts": []}
+                return data
+        except json.JSONDecodeError:
+            return {"accounts": []}
+
+    def save_tokens(self, tokens):
+        with open('tokens.json', 'w') as file:
+            json.dump(tokens, file, indent=4)
+
+    def generate_tokens(self, queries: list):
+        tokens_data = self.load_tokens()
+        accounts = tokens_data["accounts"]
+
+        for idx, query in enumerate(queries):
+            account_name = self.extract_user_data(query)
+
+            existing_account = next((acc for acc in accounts if acc["first_name"] == account_name), None)
+
+            if not existing_account:
+                print(
+                    f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
+                    f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT} {account_name} {Style.RESET_ALL}"
+                    f"{Fore.YELLOW + Style.BRIGHT}Token Is None{Style.RESET_ALL}"
+                    f"{Fore.MAGENTA + Style.BRIGHT} ] [{Style.RESET_ALL}"
+                    f"{Fore.YELLOW + Style.BRIGHT} Generating Token... {Style.RESET_ALL}"
+                    f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}",
+                    end="\r", flush=True
+                )
+                time.sleep(1)
+
+                token = self.auth(query)
+                if token:
+                    self.log(
+                        f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                        f"{Fore.WHITE + Style.BRIGHT} {account_name} {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA + Style.BRIGHT}] [{Style.RESET_ALL}"
+                        f"{Fore.GREEN + Style.BRIGHT} Successfully Generated Token {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}                           "
+                    )
+                    accounts.insert(idx, {"first_name": account_name, "token": token})
+                else:
+                    self.log(
+                        f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                        f"{Fore.WHITE + Style.BRIGHT} {account_name} {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA + Style.BRIGHT}] [{Style.RESET_ALL}"
+                        f"{Fore.RED + Style.BRIGHT} Failed to Generate Token {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}                           "
+                    )
+
+                time.sleep(1)
+                self.log(f"{Fore.CYAN + Style.BRIGHT}-{Style.RESET_ALL}" * 75)
+
+        self.save_tokens({"accounts": accounts})
+
+    def renew_token(self, account_name):
+        tokens_data = self.load_tokens()
+        accounts = tokens_data.get("accounts", [])
+        
+        account = next((acc for acc in accounts if acc["first_name"] == account_name), None)
+        
+        if account and "token" in account:
+            token = account["token"]
+            if not self.farming_status(token):
+                print(
+                    f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
+                    f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT} {account_name} {Style.RESET_ALL}"
+                    f"{Fore.RED + Style.BRIGHT}Token Isn't Valid{Style.RESET_ALL}"
+                    f"{Fore.MAGENTA + Style.BRIGHT} ] [{Style.RESET_ALL}"
+                    f"{Fore.YELLOW + Style.BRIGHT} Regenerating Token... {Style.RESET_ALL}"
+                    f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}",
+                    end="\r", flush=True
+                )
+                time.sleep(1)
+                
+                accounts = [acc for acc in accounts if acc["first_name"] != account_name]
+                
+                query = next((query for query in self.load_queries() if self.extract_user_data(query) == account_name), None)
+                if query:
+                    new_token = self.auth(query)
+                    if new_token:
+                        accounts.append({"first_name": account_name, "token": new_token})
+                        self.log(
+                            f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                            f"{Fore.WHITE + Style.BRIGHT} {account_name} {Style.RESET_ALL}"
+                            f"{Fore.MAGENTA + Style.BRIGHT}] [{Style.RESET_ALL}"
+                            f"{Fore.GREEN + Style.BRIGHT} Successfully Generated Token {Style.RESET_ALL}"
+                            f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}                           "
+                        )
+                    else:
+                        self.log(
+                            f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                            f"{Fore.WHITE + Style.BRIGHT} {account_name} {Style.RESET_ALL}"
+                            f"{Fore.MAGENTA + Style.BRIGHT}] [{Style.RESET_ALL}"
+                            f"{Fore.RED + Style.BRIGHT} Failed to Generate Token {Style.RESET_ALL}"
+                            f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}                           "
+                        )
+                else:
+                    self.log(
+                        f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                        f"{Fore.WHITE + Style.BRIGHT} {account_name} {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA + Style.BRIGHT}] [{Style.RESET_ALL}"
+                        f"{Fore.YELLOW + Style.BRIGHT} Query Is None. Skipping {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}                           "
+                    )
+
+                time.sleep(1)
+        
+        self.save_tokens({"accounts": accounts})
+
+    def load_queries(self):
+        with open('query.txt', 'r') as file:
+            return [line.strip() for line in file if line.strip()]
         
     def auth(self, query: str, retries=5, delay=3):
         url = f'https://gate.api-dormint.com/api/auth/telegram/verify?{query}'
@@ -371,46 +493,51 @@ class Dormint:
                     return None
     
     def process_query(self, query: str):
+        account_name = self.extract_user_data(query)
+    
+        tokens_data = self.load_tokens()
+        accounts = tokens_data.get("accounts", [])
 
-        account = self.load_data(query)
-
-        token = self.auth(query)
-        if not token:
+        exist_account = next((acc for acc in accounts if acc["first_name"] == account_name), None)
+        if not exist_account:
             self.log(
                 f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
-                f"{Fore.WHITE + Style.BRIGHT} {account} {Style.RESET_ALL}"
-                f"{Fore.RED + Style.BRIGHT}Token Is None{Style.RESET_ALL}"
+                f"{Fore.WHITE + Style.BRIGHT} {account_name} {Style.RESET_ALL}"
+                f"{Fore.RED + Style.BRIGHT}Token Not Found in tokens.json{Style.RESET_ALL}"
                 f"{Fore.MAGENTA + Style.BRIGHT} ]{Style.RESET_ALL}"
             )
             return
+        
+        if exist_account and "token" in exist_account:
+            token = exist_account["token"]
 
-        if token:
             farming = self.farming_status(token)
             if not farming:
-                self.log(
-                    f"{Fore.MAGENTA + Style.BRIGHT}[ Farming{Style.RESET_ALL}"
-                    f"{Fore.RED + Style.BRIGHT} Data Is None {Style.RESET_ALL}"
-                    f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
-                )
-                return
-            
+                self.renew_token(account_name)
+                tokens_data = self.load_tokens()
+                new_account = next((acc for acc in tokens_data["accounts"] if acc["first_name"] == account_name), None)
+                
+                if new_account and "token" in new_account:
+                    new_token = new_account["token"] 
+                    farming = self.farming_status(new_token)
+
             if farming:
                 self.log(
                     f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
-                    f"{Fore.WHITE + Style.BRIGHT} {account} {Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT} {account_name} {Style.RESET_ALL}"
                     f"{Fore.MAGENTA + Style.BRIGHT}] [ Balance{Style.RESET_ALL}"
                     f"{Fore.WHITE + Style.BRIGHT} {farming['sleepcoin_balance']:.1f} {Style.RESET_ALL}"
                     f"{Fore.MAGENTA + Style.BRIGHT}Sleep Coins ]{Style.RESET_ALL}"
                 )
 
-                frens = self.invited_frens(token)
+                frens = self.invited_frens(new_token if 'new_token' in locals() else token)
                 if frens:
                     for fren in frens:
                         if fren:
                             claim_token = fren['claim_secret']
 
                             if fren['balance'] != 0:
-                                claim = self.claim_frens(token, claim_token)
+                                claim = self.claim_frens(new_token if 'new_token' in locals() else token, claim_token)
                                 if claim:
                                     self.log(
                                         f"{Fore.MAGENTA+Style.BRIGHT}[ Frens{Style.RESET_ALL}"
@@ -431,7 +558,7 @@ class Dormint:
                 farming_left_seconds = float(farming_left)
                 farming_end_time = datetime.now().astimezone(wib) + timedelta(seconds=farming_left_seconds)
 
-                claim = self.claim_farming(token)
+                claim = self.claim_farming(new_token if 'new_token' in locals() else token)
                 if claim:
                     self.log(
                         f"{Fore.MAGENTA+Style.BRIGHT}[ Farming{Style.RESET_ALL}"
@@ -449,7 +576,7 @@ class Dormint:
                         f"{Fore.MAGENTA+Style.BRIGHT}]{Style.RESET_ALL}"
                     )
 
-                start = self.start_farming(token)
+                start = self.start_farming(new_token if 'new_token' in locals() else token)
                 if start:
                     self.log(
                         f"{Fore.MAGENTA+Style.BRIGHT}[ Farming{Style.RESET_ALL}"
@@ -463,7 +590,7 @@ class Dormint:
                         f"{Fore.MAGENTA+Style.BRIGHT}]{Style.RESET_ALL}"
                     )
 
-                lootboxes = self.lootboxes_status(token)
+                lootboxes = self.lootboxes_status(new_token if 'new_token' in locals() else token)
                 if lootboxes:
                     balance = lootboxes['sleepcoinBalance']
                     price = 500
@@ -480,7 +607,7 @@ class Dormint:
 
                     if balance >= price:
                         while lootboxes_left > 0:
-                            buy = self.buy_lootboxes(token)
+                            buy = self.buy_lootboxes(new_token if 'new_token' in locals() else token)
                             if buy:
                                 balance = buy['sleepcoinBalance']
                                 self.log(
@@ -507,11 +634,11 @@ class Dormint:
                             f"{Fore.MAGENTA+Style.BRIGHT}]{Style.RESET_ALL}"
                         )
 
-                    lootboxes_count = self.lootboxes_status(token)['lootboxCount']
+                    lootboxes_count = self.lootboxes_status(new_token if 'new_token' in locals() else token)['lootboxCount']
                     while lootboxes_count > 0:
-                        open = self.open_lootboxes(token)
+                        open = self.open_lootboxes(new_token if 'new_token' in locals() else token)
                         if open:
-                            lootboxes_count = self.lootboxes_status(token)['lootboxCount']
+                            lootboxes_count = self.lootboxes_status(new_token if 'new_token' in locals() else token)['lootboxCount']
                             self.log(
                                 f"{Fore.MAGENTA+Style.BRIGHT}[ Lootboxes{Style.RESET_ALL}"
                                 f"{Fore.GREEN+Style.BRIGHT} Is Opened {Style.RESET_ALL}"
@@ -538,7 +665,7 @@ class Dormint:
                         f"{Fore.MAGENTA+Style.BRIGHT}]{Style.RESET_ALL}"
                     )
 
-                quests = self.quests_list(token)
+                quests = self.quests_list(new_token if 'new_token' in locals() else token)
                 if quests:
                     for quest in quests:
 
@@ -547,7 +674,7 @@ class Dormint:
                             title = quest['name']
                             reward = quest['reward']
 
-                            start = self.start_quests(token, quest_id)
+                            start = self.start_quests(new_token if 'new_token' in locals() else token, quest_id)
                             if start:
                                 self.log(
                                     f"{Fore.MAGENTA+Style.BRIGHT}[ Quest{Style.RESET_ALL}"
@@ -564,7 +691,7 @@ class Dormint:
                                     f"{Fore.RED+Style.BRIGHT}Isn't Completed{Style.RESET_ALL}"
                                     f"{Fore.MAGENTA+Style.BRIGHT} ]{Style.RESET_ALL}"
                                 )
-                            quests = self.quests_list(token)
+                            quests = self.quests_list(new_token if 'new_token' in locals() else token)
                 else:
                     self.log(
                         f"{Fore.MAGENTA+Style.BRIGHT}[ Quest{Style.RESET_ALL}"
@@ -573,9 +700,10 @@ class Dormint:
                     )
 
     def main(self):
+        self.clear_terminal()
         try:
-            with open('query.txt', 'r') as file:
-                queries = [line.strip() for line in file if line.strip()]
+            queries = self.load_queries()
+            self.generate_tokens(queries)
 
             while True:
                 self.clear_terminal()
@@ -587,11 +715,9 @@ class Dormint:
                 self.log(f"{Fore.CYAN + Style.BRIGHT}-{Style.RESET_ALL}"*75)
 
                 for query in queries:
-                    query = query.strip()
                     if query:
                         self.process_query(query)
                         self.log(f"{Fore.CYAN + Style.BRIGHT}-{Style.RESET_ALL}"*75)
-                        time.sleep(3)
 
                 seconds = 1800
                 while seconds > 0:
